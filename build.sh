8cc356f0bc6de91eb9c4cae9807d964d2c924e86
pierpaolo <plucarelli.94@gmail.com>
2024-07-13 12:35:51 +0200

    added tests and game over func

--- build.sh
+++ build.sh
@@ -1,1 +1,12 @@
-g++ -Wall  -Wextra -I ./include *.cpp -o blokus
+#!/bin/bash
+
+
+if [ "$1" == "main" ]; then
+    g++ -Wall  -Wextra -I ./include ./src/*.cpp -o blokus
+elif [ "$1" == "test" ]; then
+    files=$(ls ./src/*.cpp | grep -v main.cpp)
+    g++ -Wall  -Wextra -I ./include ./test/test.cpp $files -o testblokus
+else
+    echo "Usage: $0 {main|test}"
+    exit 1
+fi
