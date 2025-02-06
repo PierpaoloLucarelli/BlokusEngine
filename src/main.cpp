27e999e4ac9a4322f96484e0bc13926d4ce1bd9c
Pierpaolo <plucarelli.94@gmail.com>
2025-02-06 14:29:56 +0100

    moves working

--- src/main.cpp
+++ src/main.cpp
@@ -12,12 +12,23 @@ void waitForExit() {
     std::cin >> t;
 }
 
+void printMoves(std::vector<BlokusMove> moves){
+   for(auto m : moves){
+       std::cout<<"piece: "<<std::get<0>(m) << " row: "<<std::get<1>(m)<<" col: " << std::get<2>(m)<<" Rot: " << (int)std::get<3>(m)<<std::endl;
+   }
+}
+
 int main(){
     BlokusBoard board;
     initializePieceMap();
     BlokusMatch match(board);
     match.newGame();
     match.printGame();
+
+    std::vector<BlokusMove> moves = match.getMovesFromPos(0);
+    printMoves(moves);
+
+
     bool maxPlayer = true;
     waitForExit();
     return 0;
