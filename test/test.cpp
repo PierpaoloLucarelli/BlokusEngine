a2fee342966601206a23f679ad5c107be1940d2b
Pierpaolo <plucarelli.94@gmail.com>
2025-05-02 08:30:40 +0200

    added uni test

--- test/test.cpp
+++ test/test.cpp
@@ -1,79 +1,60 @@
 #include <iostream>
 #include <match.h>
+#include <pieceShapes.h>
+#include <algorithm>
 
 std::string PASSED = "TEST PASSED";
 std::string FAILED = "TEST FAILED";
 
-namespace {
-    BlokusPiece& getPiece(blokusShapeType p){
-        auto it = piecesMap.find(p);
-        if(it == piecesMap.end()){
-            throw std::runtime_error("Piece not found in piece map");
-        }
-        return it->second;
+int rowColToFlat(int row, int col, int width){
+    return (row*width) + col;
+}
+
+bool _canPlacePiece(int pieceId, int placeRow, int placeCol, int rotation, int turn){
+    BlokusBoard board;
+    board.reset();
+    const Block& piecePlayed = rotatedPieces[pieceId];
+    board.placePiece(piecePlayed, placeRow, placeCol, rotation, turn);
+    std::vector<int> occupiedIndeces;
+    std::vector<std::pair<int, int>> coords = piecePlayed.coords[rotation];
+    for(auto coord : coords){
+        occupiedIndeces.push_back(rowColToFlat(placeRow+coord.second, placeCol+coord.first, board.getWidth()));
     }
+    for(int i = 0 ; i < board.getWidth() * board.getHeight() ; i++){
+        if (std::find(occupiedIndeces.begin(), occupiedIndeces.end(), i) != occupiedIndeces.end()) {
+            if(board.getState()[i] != turn){
+                return false;
+            }
+        } else {
+            if(board.getState()[i] != EMPTYCELL){
+                return false;
+            }
+        }
+    }
+    return true;
 }
 
-int testGameOver(){
-    BlokusBoard board;
-    initializePieceMap();
-    BlokusMatch match(board);
-    match.newGame();
-    match.playMove(blokusShapeType::zShapeType, 0, 0, true);
-    match.playMove(blokusShapeType::zShapeType, 18, 17, false);
-    match.playMove(blokusShapeType::iShapeType, 2, 3, true);
-    match.playMove(blokusShapeType::iShapeType, 17, 12, false);
-    match.playMove(blokusShapeType::smallCornerShapeType, 3, 8, true);
-    match.playMove(blokusShapeType::smallCornerShapeType, 15, 10, false);
-    match.playMove(blokusShapeType::bigCornerShapeType, 5, 7, true);
-    match.playMove(blokusShapeType::bigCornerShapeType, 12, 7, false);
-    match.playMove(blokusShapeType::singleShapeType, 1, 8, true);
-    match.playMove(blokusShapeType::singleShapeType, 15, 6, false);
-    match.playMove(blokusShapeType::crossShapeType, 8, 9, true);
-    match.playMove(blokusShapeType::crossShapeType, 12, 10, false);
-    match.playMove(blokusShapeType::tShapeType, 2, 10, true);
-    match.playMove(blokusShapeType::tShapeType, 15, 17, false);
-    match.playMove(blokusShapeType::oShapeType, 4, 13, true);
-    match.playMove(blokusShapeType::oShapeType, 13, 16, false);
-
-    match.getBoard().printBoardState();
-
-    return match.gameOver(true);
-
-}
-
-int testRotatePiece(){
-    BlokusBoard board;
-    initializePieceMap();
-    BlokusMatch match(board);
-    match.newGame();
-
-    BlokusPiece& p = getPiece(blokusShapeType::longlShapeType);
-    match.getBoard().placePiece(p, 5, 5, true);
-    match.getBoard().printBoardState();
-    match.getBoard().removePiece(p, 5, 5);
-    p.rotate();
-    match.getBoard().placePiece(p, 5, 5, true);
-    match.getBoard().printBoardState();
-    match.getBoard().removePiece(p, 5, 5);
-    p.rotate();
-    match.getBoard().placePiece(p, 5, 5, true);
-    match.getBoard().printBoardState();
-    match.getBoard().removePiece(p, 5, 5);
-    p.rotate();
-    match.getBoard().placePiece(p, 5, 5, true);
-    match.getBoard().printBoardState();
-    match.getBoard().removePiece(p, 5, 5);
-    return 1;
+bool testCanPlaceAllPieces(){
+    int placeRow = 10;
+    int placeCol = 10;
+    int turn = 0;
+    int rotation = 0;
+    for(int pieceId = 0 ; pieceId < 21 ; pieceId++){
+        if(!_canPlacePiece(pieceId, placeRow, placeCol, rotation, turn)){
+            std::cout << pieceId << std::endl;
+            return false;
+        }
+    }
+    return true;
 }
 
 int main(){
     std::cout << "Running tests." << std::endl;
 
-    if(testRotatePiece()){
-        std::cout << "testGameOver(): " + PASSED << std::endl;
+    if(testCanPlaceAllPieces()){
+        std::cout << "testCanPlaceAllPieces(): " + PASSED << std::endl;
     } else{
-        std::cout << "testGameOver(): " + FAILED << std::endl;
+        std::cout << "testCanPlaceAllPieces(): " + FAILED << std::endl;
     }
     return 0;
 
