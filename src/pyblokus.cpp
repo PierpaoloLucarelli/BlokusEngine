1459d92bfdb9a123d0ece7c2ad6d4c73f5886465
Pierpaolo <plucarelli.94@gmail.com>
2025-05-05 11:30:45 +0200

    added getTurn function to pyinding

--- src/pyblokus.cpp
+++ src/pyblokus.cpp
@@ -13,32 +13,9 @@ PYBIND11_MODULE(pyblokus, m) {
         .def("removeMove", &BlokusMatch::removeMove)
         .def("getMovesFromPos", &BlokusMatch::getMovesFromPos)
         .def("getCornersFromPos", &BlokusMatch::getCornersFromPos)
+        .def("getTurn", &BlokusMatch::getTurn)
         .def("getState", &BlokusMatch::getState, "Returns the state of the board as a list of integers");
-
-        // py::enum_<blokusShapeType>(m, "blokusShapeType")
-        // .value("iShapeType", blokusShapeType::iShapeType)
-        // .value("bigCornerShapeType", blokusShapeType::bigCornerShapeType)
-        // .value("crossShapeType", blokusShapeType::crossShapeType)
-        // .value("snakeShapeType", blokusShapeType::snakeShapeType)
-        // .value("dinoShapeType", blokusShapeType::dinoShapeType)
-        // .value("submarineShapeType", blokusShapeType::submarineShapeType)
-        // .value("zigzagShapeType", blokusShapeType::zigzagShapeType)
-        // .value("bigtShapeType", blokusShapeType::bigtShapeType)
-        // .value("uShapeType", blokusShapeType::uShapeType)
-        // .value("longlShapeType", blokusShapeType::longlShapeType)
-        // .value("diggerShapeType", blokusShapeType::diggerShapeType)
-        // .value("sixShapeType", blokusShapeType::sixShapeType)
-        // .value("zShapeType", blokusShapeType::zShapeType)
-        // .value("oShapeType", blokusShapeType::oShapeType)
-        // .value("tShapeType", blokusShapeType::tShapeType)
-        // .value("mediumlShapeType", blokusShapeType::mediumlShapeType)
-        // .value("threeShapeType", blokusShapeType::threeShapeType)
-        // .value("smallCornerShapeType", blokusShapeType::smallCornerShapeType)
-        // .value("twoShapeType", blokusShapeType::twoShapeType)
-        // .value("singleShapeType", blokusShapeType::singleShapeType)
-        // .value("passShapeType", blokusShapeType::passShapeType)
-        // .export_values(); // Allow the values to be used in Python
-
+        
     // Expose the BlokusMove as a tuple (shape, row, col, rotation)
     m.def("BlokusMove", [](blokusShapeType shape, int row, int col, uint8_t rotation) {
         return std::make_tuple(shape, row, col, rotation);
